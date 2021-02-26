419da2c69585d489ecaf5e1f37112cb12467011c
Jay Acuna <jtacuna@ucsd.edu>
2021-02-25 17:27:07 -0800

    thirdPartyAPI lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,26 +2,49 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-  initializePage();
-})
-
-/*
- * Function that is called when the document is ready.
- */
-function initializePage() {
-	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
- 	initGestures();
- 	initRSVPForm();
-}
-
-// init jQuery gestures  
-function initGestures() {
-	// add gestures listener here
-}
-
-// init RSVP form submit listener
-function initRSVPForm() {
-  // add your code here
-}
+	initializePage();
+  })
+  
+  /*
+   * Function that is called when the document is ready.
+   */
+  function initializePage() {
+	  console.log("Page ready");
+	   initCamera();
+	   initMap();
+	   initGestures();
+	   initRSVPForm();
+  }
+  
+  // init jQuery gestures  
+  function initGestures() {
+	  // add gestures listener here
+	  $(function(){
+			  $(".judge-img").bind("taphold", tapholdHandler);
+  
+			  function tapholdHandler(event){
+				  // get the id of the evrent source 
+				  var targetIDPrefix = event.target.id;
+				  console.log("got prefix: " + targetIDPrefix);
+				  //show bio
+				  $("#" + targetIDPrefix + "-bio").show(); 
+			  }
+	  })
+  }
+  
+  // init RSVP form submit listener
+  function initRSVPForm() {
+	// add your code here
+	$('#rsvpForm').submit(function(e) {
+		  e.preventDefault();
+		  console.log("submitting for...");
+		  var rsvpEmail = $('#rsvpEmail').val();
+  
+		  $.post('addRSVP', { rsvpEmail: rsvpEmail }, postCallback);
+	});
+  
+	function postCallback(res) {
+		alert("RSVP form successfully submitted!");
+		$('#rsvpEmail').val('');
+	}
+  }
